019e4c6ddb3718ec1ac24be35fd442b138119be0
Akash Tiwari <154494591+NOTHINGISSECURE@users.noreply.github.com>
2026-04-08 12:22:11 +0530

    08 april 2026

---
+++ 07string01/02assignment/01.cpp
@@ -0,0 +1,7 @@
+// Input a string of size n and update all the odd positions in the string to character ‘#’. Consider 0-based indexing.
+
+// Input : str = "Pbwcshkuiglhlds"
+// Output : "P#w#s#k#i#l#l#s"
+
+// Input : str = "a"
+// Output : "a"
