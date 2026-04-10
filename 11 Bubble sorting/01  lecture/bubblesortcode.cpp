5878e5819c4db8aa23492fec4ca7b542dd8f41f4
Akash Tiwari <154494591+NOTHINGISSECURE@users.noreply.github.com>
2026-04-10 12:11:02 +0530

    11 april 2026

--- 11 Bubble sorting/01  lecture/bubblesortcode.cpp
+++ 11 Bubble sorting/01  lecture/bubblesortcode.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-bool isSorted(int arr[],int n){
+bool isSorted(int arr[],int n){//to check if the array is already sorted or not
     for(int i=0;i<n-1;i++){
         if(arr[i]>arr[i+1]){
             return false;
@@ -12,8 +12,7 @@ int main(){
     int n=5;
     int arr[] = { 5,4,3,2,1};
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-        
+        cout<<arr[i]<<" ";     
     }
     cout<<endl;
     for(int i=0;i<n-1;i++){
@@ -26,14 +25,28 @@ int main(){
             }
         }
     }
-    
-    // Print the sorted array
+
+    // optimised bubble sort
+        for(int i=0;i<n-1;i++){
+            bool flag=true;
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    // int temp = arr[j]; // swapping the elements using temp function
+                    // arr[j] = arr[j+1];
+                    // arr[j+1] = temp;
+                    swap(arr[j], arr[j+1]); //swapping the elements using swap function
+                }
+            }
+            if(flag==true){
+                break;
+            }
+        }
+
+//print the sorted array
     cout << "Sorted array: ";
     for(int i=0;i<n;i++){
         cout << arr[i] << " ";
     }
     cout << endl;
-    
-    return 0;
 }
 
---
+++ 11 Bubble sorting/02 assignment/q4.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    int arr[] = { 2,3,4,5,7};
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";     
+    }
+    cout<<endl;
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]<arr[j+1]){
+                // int temp = arr[j]; // swapping the elements using temp function
+                // arr[j] = arr[j+1];
+                // arr[j+1] = temp;
+                swap(arr[j], arr[j+1]); //swapping the elements using swap function
+            }
+        }
+    }
+//print the sorted array in non increasing order
+    cout << "Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
