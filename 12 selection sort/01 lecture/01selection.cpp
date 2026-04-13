787e58426eb01a435add3cddc1e95c5db4573879
Akash Tiwari <154494591+NOTHINGISSECURE@users.noreply.github.com>
2026-04-13 21:15:52 +0530

    13 april 2026

---
+++ 12 selection sort/01 lecture/01selection.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+#include<climits>
+using namespace std;
+int main(){
+    int arr[] = { 1,3,4,5,2};
+    int n=5;
+    for( int ele : arr){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+    for (int i=0;i<n-1;i++){
+        int max= INT_MAX;
+        int minx=-1;
+        for(int j=i;j<n;j++){
+            if(arr[j]<max){
+                max=arr[j];
+                minx=j;
+            }
+        }
+        swap(arr[i],arr[minx]);
+    }
+
+    for (int ele:arr){
+        cout<<ele<<" ";
+    }
+}
