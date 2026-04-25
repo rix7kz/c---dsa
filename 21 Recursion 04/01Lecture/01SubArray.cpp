acbaa31820759e3baab2ab30d7fd261dd7e576a7
Akash Tiwari <154494591+NOTHINGISSECURE@users.noreply.github.com>
2026-04-25 11:28:28 +0530

    25 april 2026

--- 21 Recursion 04/01Lecture/01SubArray.cpp
+++ 21 Recursion 04/01Lecture/01SubArray.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 void suba(vector<int>v ,int arr[],int n,int idx){
-    
+    // base case
+    if(n==idx){
+        for(int i=0;i<v.size();i++){
+            cout<<v[i];
+        }
+            cout<<endl;
+            return;
+    }
+    //kaam
+    suba(v,arr,n,idx+1);
+    if(v.size()==0  ||  arr[idx-1] == v[v.size()-1]){   //will work in unique elements only
+        v.push_back(arr[idx]);
+        suba(v,arr,n,idx+1);
+    }
+
 }
 int main(){
     int arr[]= {1,2,3,4};
     int n = sizeof(arr)/sizeof(arr[0]);
     // cout<<n;
+    vector<int>v;
     suba(v,arr,n,0);
 }
