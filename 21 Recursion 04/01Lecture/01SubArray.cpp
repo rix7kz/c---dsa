09fd5acee58b930ac64c70be137e3766096f030f
Akash Tiwari <154494591+NOTHINGISSECURE@users.noreply.github.com>
2026-04-25 11:17:46 +0530

    25 april 2026

---
+++ 21 Recursion 04/01Lecture/01SubArray.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void suba(vector<int>v ,int arr[],int n,int idx){
+    
+}
+int main(){
+    int arr[]= {1,2,3,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    // cout<<n;
+    suba(v,arr,n,0);
+}
